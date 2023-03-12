e2f5aeaf87f9fea726f42b82fa4f3a0c5d795182
Mustafa Bekirov <mustafos1999@icloud.com>
2023-03-12 12:35:27 +0100

    Create layout

---
+++ LearnApp/LearnApp/LearnAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  LearnAppApp.swift
+//  LearnApp
+//
+//  Created by Mustafa Bekirov on 12.03.2023.
+//
+
+import SwiftUI
+
+@main
+struct LearnAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
