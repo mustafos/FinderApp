6642af33709878dad262af317967ca97eb97702a
Mustafa Bekirov <mustafos1999@icloud.com>
2023-03-19 21:09:06 +0100

    fetch: refactoring structure

--- LearnApp/AccountView.swift
+++ LearnApp/AccountView.swift
@@ -8,74 +8,112 @@
 import SwiftUI
 
 struct AccountView: View {
+    @State var isDeleted = false
+    @State var isPinned = false
+    
     var body: some View {
         NavigationView {
             List {
-                VStack(spacing: 8) {
-                    Image(systemName: "person.crop.circle.fill.badge.checkmark")
-                        .symbolVariant(.circle.fill)
-                        .font(.system(size: 32))
-                        .symbolRenderingMode(.palette)
-                        .foregroundStyle(.blue, .blue.opacity(0.3))
-                        .padding()
-                        .background(Circle().fill(.ultraThinMaterial))
-                        .background(Image(systemName: "hexagon")
-                            .symbolVariant(.fill)
-                            .foregroundColor(.blue)
-                            .font(.system(size: 200))
-                            .offset(x: -50, y: -100)
-                        )
-                    Text("Mustafa Bekirov")
-                        .font(.title.weight(.semibold))
-                    HStack {
-                        Image(systemName: "location")
-                            .imageScale(.large)
-                        Text("Czech Republic")
-                            .foregroundColor(.secondary)
-                    }
-                }
-                .frame(maxWidth: .infinity)
-                .padding()
+                profile
                 
-                Section {
-                    NavigationLink(destination: ContentView()) {
-                        Label("Settings", systemImage: "gear")
-                    }
-                    NavigationLink { Text("Billing") } label: {
-                        Label("Billing", systemImage: "creditcard")
-                    }
-                    NavigationLink { ContentView() } label: {
-                        Label("Help", systemImage: "questionmark")
-                    }
-                }
-                .accentColor(.primary)
-                .listRowSeparatorTint(.blue)
-                .listRowSeparator(.hidden)
+                menu
                 
-                Section {
-                    Link(destination: URL(string: "https://mustafos.github.io")!) {
-                        HStack {
-                            Label("Website", systemImage: "house")
-                            Spacer()
-                            Image(systemName: "link")
-                                .foregroundColor(.secondary)
-                        }
-                    }
-                    Link(destination: URL(string: "https://github.com/mustafos")!) {
-                        HStack {
-                            Label("GitHub", systemImage: "tv")
-                            Spacer()
-                            Image(systemName: "link")
-                                .foregroundColor(.secondary)
-                        }
-                    }
-                }
-                .accentColor(.primary)
-                .listRowSeparator(.hidden)
+                links
             }
             .listStyle(.insetGrouped)
             .navigationTitle("Account")
         }
+    }
+    
+    var profile: some View {
+        VStack(spacing: 8) {
+            Image(systemName: "person.crop.circle.fill.badge.checkmark")
+                .symbolVariant(.circle.fill)
+                .font(.system(size: 32))
+                .symbolRenderingMode(.palette)
+                .foregroundStyle(.blue, .blue.opacity(0.3))
+                .padding()
+                .background(Circle().fill(.ultraThinMaterial))
+                .background(Image(systemName: "hexagon")
+                    .symbolVariant(.fill)
+                    .foregroundColor(.blue)
+                    .font(.system(size: 200))
+                    .offset(x: -50, y: -100)
+                )
+            Text("Mustafa Bekirov")
+                .font(.title.weight(.semibold))
+            HStack {
+                Image(systemName: "location")
+                    .imageScale(.large)
+                Text("Czech Republic")
+                    .foregroundColor(.secondary)
+            }
+        }
+        .frame(maxWidth: .infinity)
+        .padding()
+    }
+     
+    var menu: some View {
+        Section {
+            NavigationLink(destination: ContentView()) {
+                Label("Settings", systemImage: "gear")
+            }
+            NavigationLink { Text("Billing") } label: {
+                Label("Billing", systemImage: "creditcard")
+            }
+            NavigationLink { ContentView() } label: {
+                Label("Help", systemImage: "questionmark")
+            }
+        }
+        .accentColor(.primary)
+        .listRowSeparatorTint(.blue)
+        .listRowSeparator(.hidden)
+    }
+    
+    var links: some View {
+        Section {
+            if !isDeleted {
+                Link(destination: URL(string: "https://mustafos.github.io")!) {
+                    HStack {
+                        Label("Website", systemImage: "house")
+                        Spacer()
+                        Image(systemName: "link")
+                            .foregroundColor(.secondary)
+                    }
+                }
+                .swipeActions(edge: .trailing, allowsFullSwipe: true) {
+                    Button(action: { isDeleted = true }) {
+                        Label("Delete", systemImage: "trash")
+                    }
+                    .tint(.red)
+                    pinButton
+                }
+            }
+            Link(destination: URL(string: "https://github.com/mustafos")!) {
+                HStack {
+                    Label("GitHub", systemImage: "tv")
+                    Spacer()
+                    Image(systemName: "link")
+                        .foregroundColor(.secondary)
+                }
+            }
+            .swipeActions {
+                pinButton
+            }
+        }
+        .accentColor(.primary) // fix bug to new version 'color icon black'
+        .listRowSeparator(.hidden)
+    }
+    
+    var pinButton: some View {
+        Button { isPinned.toggle() } label: {
+            if isPinned {
+                Label("Unpin", systemImage: "pin.slash")
+            } else {
+                Label("Pin", systemImage: "pin")
+            }
+        }
+        .tint(isPinned ? .gray : .yellow)
     }
 }
 
