f79628c92b1f465120c3593e8f65ebbf85eb9908
Mustafa Bekirov <mustafos1999@icloud.com>
2023-03-21 19:16:43 +0100

    Add graphics canvas

--- LearnApp/AccountView.swift
+++ LearnApp/AccountView.swift
@@ -34,10 +34,8 @@ struct AccountView: View {
                 .foregroundStyle(.blue, .blue.opacity(0.3))
                 .padding()
                 .background(Circle().fill(.ultraThinMaterial))
-                .background(Image(systemName: "hexagon")
-                    .symbolVariant(.fill)
-                    .foregroundColor(.blue)
-                    .font(.system(size: 200))
+                .background(
+                    HexagonView()
                     .offset(x: -50, y: -100)
                 )
             Text("Mustafa Bekirov")
