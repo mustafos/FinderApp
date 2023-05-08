6378dcf196ba9103599a606123959567618414b8
Mustafa Bekirov <103932078+mustafos@users.noreply.github.com>
2023-05-08 20:50:17 +0200

    feat: string logic with ui

--- FinderApp/Model/Model.swift
+++ FinderApp/Model/Model.swift
@@ -5,27 +5,49 @@
 //  Created by Mustafa Bekirov on 27.03.2023.
 //
 
-import SwiftUI
-import Combine
+import Foundation
 
-class Model: ObservableObject {
-    // Tab Bar
-    @Published var showTab: Bool = true
+class ReverseWordsModel {
     
-    // Navigation Bar
-    @Published var showNav: Bool = true
+    func reverseWords(in inputString: String) -> String {
+        let words = inputString.components(separatedBy: " ")
+        let reversedWords = words.map { String($0.reversed()) }
+        return reversedWords.joined(separator: " ")
+    }
     
-    // Modal
-    @Published var selectedModal: Modal = .signUp
-    @Published var showModal: Bool = false
-    @Published var dismissModal: Bool = false
+    func reverseExceptAlphabeticSymbols(in inputString: String) -> String {
+        let words = inputString.components(separatedBy: " ")
+        let reversedWords = words.map { word -> String in
+            let letters = word.filter { $0.isLetter }
+            var reversedLetters = String(letters.reversed())
+            var result = ""
+            for c in word {
+                if c.isLetter {
+                    result.append(reversedLetters.removeFirst())
+                } else {
+                    result.append(c)
+                }
+            }
+            return result
+        }
+        return reversedWords.joined(separator: " ")
+    }
     
-    // Detail View
-    @Published var showDetail: Bool = false
-    @Published var selectedCourse: Int = 0
-}
-
-enum Modal: String {
-    case signUp
-    case signIn
+    func reverseIgnoredUserDefined(in inputString: String, ignoredChars: Set<Character> = []) -> String {
+        let words = inputString.components(separatedBy: " ")
+        let reversedWords = words.map { word -> String in
+            let letters = word.filter { !ignoredChars.contains($0) && $0.isLetter }
+            var reversedLetters = String(letters.reversed())
+            var result = ""
+            for c in word {
+                if !ignoredChars.contains(c) && c.isLetter {
+                    result.append(reversedLetters.removeFirst())
+                } else {
+                    result.append(c)
+                }
+            }
+            return result
+        }
+        return reversedWords.joined(separator: " ")
+    }
 }
