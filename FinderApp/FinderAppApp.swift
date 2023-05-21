dcc76bfcd4b57d59f191de1049823247bd7ff868
Mustafa Bekirov <mustafos1999@icloud.com>
2023-05-21 20:45:59 +0200

    init new project

---
+++ FinderApp/FinderAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  FinderAppApp.swift
+//  FinderApp
+//
+//  Created by Mustafa Bekirov on 21.05.2023.
+//
+
+import SwiftUI
+
+@main
+struct FinderAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
