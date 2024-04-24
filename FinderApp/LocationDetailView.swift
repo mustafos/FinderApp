4e17f4105f95d339e1d08ca2ce05f4fec7daf388
mustafos <mustafos1999@icloud.com>
2024-04-25 00:02:16 +0200

    feat: nav link with scroll view

--- FinderApp/LocationDetailView.swift
+++ FinderApp/LocationDetailView.swift
@@ -10,65 +10,66 @@ import SwiftUI
 struct LocationDetailView: View {
     
     let columns = [GridItem(.flexible()),
-                  GridItem(.flexible()),
-                  GridItem(.flexible())]
+                   GridItem(.flexible()),
+                   GridItem(.flexible())]
     var body: some View {
-        NavigationView {
-            VStack(spacing: 16) {
-                Image("default-banner-asset")
-                    .resizable()
-                    .scaledToFill()
-                    .frame(height: 120)
+        VStack(spacing: 16) {
+            Image("default-banner-asset")
+                .resizable()
+                .scaledToFill()
+                .frame(height: 120)
+            
+            HStack {
+                Label("123 Main Screen", systemImage: "mappin.and.ellipse")
+                    .font(.caption)
+                    .foregroundColor(.secondary)
                 
-                HStack {
-                    Label("123 Main Screen", systemImage: "mappin.and.ellipse")
-                        .font(.caption)
-                        .foregroundColor(.secondary)
-                    
-                    Spacer()
-                }
+                Spacer()
+            }
+            .padding(.horizontal)
+            
+            Text("There are many variations of passages of Lorem Ipsum available, but the majority have suffered alteration in some form, by injected humour, or randomised words which don't look even slightly believable.")
+                .lineLimit(3)
+                .minimumScaleFactor(0.75)
+                .frame(height: 70)
                 .padding(.horizontal)
+            
+            ZStack {
+                Capsule()
+                    .frame(height: 80)
+                    .foregroundColor(Color(.secondarySystemBackground))
                 
-                Text("There are many variations of passages of Lorem Ipsum available, but the majority have suffered alteration in some form, by injected humour, or randomised words which don't look even slightly believable.")
-                    .lineLimit(3)
-                    .minimumScaleFactor(0.75)
-                    .padding(.horizontal)
-                
-                ZStack {
-                    Capsule()
-                        .frame(height: 80)
-                        .foregroundColor(Color(.secondarySystemBackground))
+                HStack(spacing: 20) {
+                    Button {
+                        
+                    } label: {
+                        LocationActionButton(color: .brandPrimary, imageName: "location.fill")
+                    }
                     
-                    HStack(spacing: 20) {
-                        Button {
-                            
-                        } label: {
-                            LocationActionButton(color: .brandPrimary, imageName: "location.fill")
-                        }
+                    Link(destination: URL(string: "https://www.apple.com")!, label: {
+                        LocationActionButton(color: .brandPrimary, imageName: "network")
+                    })
+                    
+                    Button {
                         
-                        Link(destination: URL(string: "https://www.apple.com")!, label: {
-                            LocationActionButton(color: .brandPrimary, imageName: "network")
-                        })
+                    } label: {
+                        LocationActionButton(color: .brandPrimary, imageName: "phone.fill")
+                    }
+                    
+                    Button {
                         
-                        Button {
-                            
-                        } label: {
-                            LocationActionButton(color: .brandPrimary, imageName: "phone.fill")
-                        }
-                        
-                        Button {
-                            
-                        } label: {
-                            LocationActionButton(color: .red, imageName: "person.fill.checkmark")
-                        }
+                    } label: {
+                        LocationActionButton(color: .red, imageName: "person.fill.checkmark")
                     }
                 }
-                .padding(.horizontal)
-                
-                Text("Who's Here?")
-                    .bold()
-                    .font(.title2)
-                
+            }
+            .padding(.horizontal)
+            
+            Text("Who's Here?")
+                .bold()
+                .font(.title2)
+            
+            ScrollView {
                 LazyVGrid(columns: columns, content: {
                     FirstNameAvatarView(firstName: "Mustafa")
                     FirstNameAvatarView(firstName: "Kelly")
@@ -78,17 +79,18 @@ struct LocationDetailView: View {
                     FirstNameAvatarView(firstName: "Brian")
                     FirstNameAvatarView(firstName: "Smit")
                 })
-                
-                Spacer()
             }
-            .navigationTitle("Location Name")
-            .navigationBarTitleDisplayMode(.inline)
+            Spacer()
         }
+        .navigationTitle("Location Name")
+        .navigationBarTitleDisplayMode(.inline)
     }
 }
 
 #Preview {
-    LocationDetailView()
+    NavigationView {
+        LocationDetailView()
+    }
 }
 
 struct LocationActionButton: View {
---
+++ FinderApp/TextExtension.swift
@@ -0,0 +1,8 @@
+//
+//  TextExtension.swift
+//  FinderApp
+//
+//  Created by Mustafa Bekirov on 24.04.2024.
+//
+
+import Foundation
